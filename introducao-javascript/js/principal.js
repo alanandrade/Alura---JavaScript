a48b93a76e682a622b74a81ba81e36c8dbe77580
Alan Andrade <alan.andradebispo@gmail.com>
2017-12-13 02:53:28 -0200

    ajustes validacoes peso e altura

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -12,5 +12,27 @@ var peso = tdPeso.textContent;//Somente o conteudo da classe .info-peso
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-console.log(imc);
+// IMC Selecionado
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+// VALIDACAO
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if(pesoEhValido && alturaEhValida){ //Exibi IMC somente se as duas variaveis continuam true
+    // Calculo IMC e insercao no HTML
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
