6233e9f2157357d261a4cdcd6152d7005e51e180
Alan Andrade <alan.andradebispo@gmail.com>
2017-12-14 20:43:24 -0200

    ajustes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -5,10 +5,12 @@ titulo.textContent = "Aparecida Nutricionista";
 var pacientes = document.querySelectorAll(".paciente"); //Mostra todo o TR do paciente
 
 for (var i = 0; i < pacientes.length; i++) { // Se I for menor que pacientes, acrescenta mais um no I, length verifica a quantidade de itens
-    //console.log(pacientes[i]); //var I(Indice) entre [] pega cada indice do array
+
+    //console.log(pacientes); //Traz todos os elementos do Array 5x
+    //console.log(pacientes[i]); //Traz todos os elementos separados pelo loop que foi realizado
 
     var paciente = pacientes[i]; //Adiciona a cada iteracao do FOR o indice dentro de paciente
-    //Adicionando o I(Indice) nos pacientes, traz separadamente cada indice do loop separado
+    //Indice[i] acessa cada posicao do array e adiciona em pacientes
 
     // PESO
     var tdPeso = paciente.querySelector(".info-peso"); //Somente a classe HTML do .info-peso
@@ -28,16 +30,18 @@ for (var i = 0; i < pacientes.length; i++) { // Se I for menor que pacientes, ac
     if (peso <= 0 || peso >= 1000) {
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3) {
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (pesoEhValido == true && alturaEhValida == true) { //Faz o calculo IMC somente se as duas variaveis continuam true
         // Calculo IMC e insercao no HTML
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        tdImc.textContent = imc.toFixed(0);
     }
 }
