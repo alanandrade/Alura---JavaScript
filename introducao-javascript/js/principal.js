c16fd75064b5ffb5b1b5ba841ab11ad810fe37a8
Alan Andrade <alan.andradebispo@gmail.com>
2017-12-14 13:03:42 -0200

    ajustes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -4,10 +4,11 @@ titulo.textContent = "Aparecida Nutricionista";
 //Seleciona o paciente
 var pacientes = document.querySelectorAll(".paciente"); //Mostra todo o TR do paciente
 
-for (var i = 0; i < pacientes.length; i++) { // Se I for menor que pacientes, acrescenta mais um no I
+for (var i = 0; i < pacientes.length; i++) { // Se I for menor que pacientes, acrescenta mais um no I, length verifica a quantidade de itens
     //console.log(pacientes[i]); //var I(Indice) entre [] pega cada indice do array
 
     var paciente = pacientes[i]; //Adiciona a cada iteracao do FOR o indice dentro de paciente
+    //Adicionando o I(Indice) nos pacientes, traz separadamente cada indice do loop separado
 
     // PESO
     var tdPeso = paciente.querySelector(".info-peso"); //Somente a classe HTML do .info-peso
