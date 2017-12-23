41b09d409908db7e96a80149f82aa2be0f3bca5f
Alan Andrade <alan.andradebispo@gmail.com>
2017-12-23 01:46:02 -0200

    capturando valor de inputs, criando elementos, adicionando valor capturado na tabela, prevenir evento padrao do HTML e etc

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -49,5 +49,37 @@ for (var i = 0; i < pacientes.length; i++) { // Se I for menor que pacientes, ac
 var botaoAdicionar = document.getElementById("adicionar-paciente");
 botaoAdicionar.addEventListener("click",function(event){
     event.preventDefault(); //Previne o comportamento padrao de um btn de formulario
-    console.log("Botao de adicionar clicado!");
+    
+    var form = document.querySelector("#form-adiciona");
+    
+    //Adicionando valor na variavel = (dentro do form.name.valor do input)
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    //Cria a variavel e adiciona a elas o criacao de linha e coluna HTML
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    //Adiciona os valores das variaveis acima dentro do conteudo de texto dos TD
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //(appendChild) - Adicionar TD como filho de TR
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    //Seleciona tabela de exibicao
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
 })
